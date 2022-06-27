e2075d6951311cc236fb50eadfd2855a2878145e
supratim78 <45755585+supratim78@users.noreply.github.com>
2022-06-27 15:55:27 +0530

    Add files via upload

---
+++ eauction-house-configuration-service/src/test/java/com/cts/eauction/microservices/configuration/EauctionHouseConfigurationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.eauction.microservices.configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EauctionHouseConfigurationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
