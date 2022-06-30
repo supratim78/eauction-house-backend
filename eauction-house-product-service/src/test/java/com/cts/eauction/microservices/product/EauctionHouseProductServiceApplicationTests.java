65f1593cb52179b6175bfe7141d2b4fcfda13a28
unknown <cogjava254@iiht.tech>
2022-06-30 21:17:29 +0530

    commit

--- eauction-house-product-service/src/test/java/com/cts/eauction/microservices/product/EauctionHouseProductServiceApplicationTests.java
+++ eauction-house-product-service/src/test/java/com/cts/eauction/microservices/product/EauctionHouseProductServiceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EauctionHouseProductServiceApplicationTests {
 
-	@Test
+	/*@Test
 	void contextLoads() {
-	}
+	}*/
 
 }
--- eauction-house-seller-service/src/test/java/com/cts/eauction/microservices/seller/EauctionHouseSellerServiceApplicationTests.java
+++ eauction-house-seller-service/src/test/java/com/cts/eauction/microservices/seller/EauctionHouseSellerServiceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EauctionHouseSellerServiceApplicationTests {
 
-	@Test
+	/*@Test
 	void contextLoads() {
-	}
+	}*/
 
 }
