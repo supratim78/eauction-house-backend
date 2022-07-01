6fc2609ceeafee270c36d619be911866ba000397
unknown <cogjava254@iiht.tech>
2022-07-01 12:50:24 +0530

    t

--- eauction-house-seller-service/src/main/resources/EAUCTION_HOUSE_SELLER.sql
+++ eauction-house-seller-service/src/main/resources/EAUCTION_HOUSE_SELLER.sql
@@ -1,3 +1,17 @@
 CREATE SCHEMA `eauction_house_seller` ;
 create user 'dev'@'%' identified by 'password1'; -- Creates the user;
 grant all on eauction_house_seller.* to 'dev'@'%'; -- Gives all privileges to the new user on the newly created database
+
+CREATE TABLE `seller` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `FIRST_NAME` varchar(100) DEFAULT NULL,
+  `LAST_NAME` varchar(100) DEFAULT NULL,
+  `ADDRESS` varchar(200) DEFAULT NULL,
+  `CITY` varchar(45) DEFAULT NULL,
+  `STATE` varchar(45) DEFAULT NULL,
+  `PIN` varchar(10) DEFAULT NULL,
+  `PHONE` varchar(10) DEFAULT NULL,
+  `EMAIL` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `ID_UNIQUE` (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=57 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Holds the registration data for seller';
